073bcc4d97de0cfdfaec0ac6d7c9a6b4ef4b875e
austin2408 <sdnd93612@gmial.com>
2022-01-20 15:54:34 +0800

    update_docker

---
+++ Docker/docker_join.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+BASH_OPTION=bash
+
+IMG=medical_bed/handover:dualarm
+
+xhost +
+containerid=$(docker ps -aqf "ancestor=${IMG}") && echo $containerid
+docker exec -it \
+    --privileged \
+    -e DISPLAY=${DISPLAY} \
+    -e LINES="$(tput lines)" \
+    ${containerid} \
+    $BASH_OPTION
+xhost -
