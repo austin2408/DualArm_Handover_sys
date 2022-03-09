264253a0d1a3a12087751ef2d5512caa486deaf3
austin2408 <sdnd93612@gmial.com>
2022-03-09 16:05:12 +0800

    add dynamic passive

--- catkin_ws/src/handover/src/handover_client.py
+++ catkin_ws/src/handover/src/handover_client.py
@@ -3,12 +3,14 @@
 import rospy
 import smach
 import smach_ros
+from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
+import sys
 
 from smach_tutorials.msg import TestAction, TestGoal
 from actionlib import *
 from actionlib_msgs.msg import *
 
-def main():
+def static_passive():
     rospy.init_node('handover_client')
 
     sm0 = smach.StateMachine(outcomes=['succeeded','aborted','preempted', 'End'])
@@ -31,7 +33,7 @@ def main():
 
         smach.StateMachine.add('Grasp_back',
                                smach_ros.SimpleActionState('handover_action', TestAction,
-                               goal = TestGoal(goal=4)),
+                               goal = TestGoal(goal=3)),
                                {'succeeded':'End','aborted':'Grasp_back'})
 
     sis = smach_ros.IntrospectionServer('my_smach_introspection_server', sm0, '/SM_ROOT')
@@ -42,5 +44,52 @@ def main():
     rospy.spin()
     sis.stop()
 
+def dynamic_passive():
+    rospy.init_node('handover_client')
+    r = TriggerRequest()
+
+    try:
+        switch = rospy.ServiceProxy("/handover_server/switch_loop", Trigger)
+        resp = switch(r)
+    except rospy.ServiceException as exc:
+        print("service did not process request: " + str(exc))
+
+    sm0 = smach.StateMachine(outcomes=['succeeded','aborted','preempted', 'End'])
+
+    with sm0:
+        smach.StateMachine.add('Init',
+                               smach_ros.SimpleActionState('handover_action', TestAction,
+                               goal = TestGoal(goal=0)),
+                               {'succeeded':'Detect','aborted':'Init'})
+
+        smach.StateMachine.add('Detect',
+                               smach_ros.SimpleActionState('handover_action', TestAction,
+                               goal = TestGoal(goal=1)),
+                               {'succeeded':'Move_to','aborted':'Detect'})
+
+        smach.StateMachine.add('Move_to',
+                               smach_ros.SimpleActionState('handover_action', TestAction,
+                               goal = TestGoal(goal=2)),
+                               {'succeeded':'Check_dis','aborted':'Move_to'})
+
+        smach.StateMachine.add('Check_dis',
+                               smach_ros.SimpleActionState('handover_action', TestAction,
+                               goal = TestGoal(goal=4)),
+                               {'succeeded':'Grasp_back','aborted':'Detect'})
+
+        smach.StateMachine.add('Grasp_back',
+                               smach_ros.SimpleActionState('handover_action', TestAction,
+                               goal = TestGoal(goal=3)),
+                               {'succeeded':'End','aborted':'Grasp_back'})
+
+    sis = smach_ros.IntrospectionServer('my_smach_introspection_server', sm0, '/SM_ROOT')
+    sis.start()
+
+    outcome = sm0.execute()
+
+    rospy.spin()
+    sis.stop()
+    
 if __name__ == '__main__':
-    main()
+    # dynamic_passive()
+    static_passive()
